75b66ab61cc276ca2da5655dcb624de52cf721e3
Christopher Suh <ChrisSuh@ChristophersMacbookPro.local>
2017-03-14 17:51:42 -0400

    stems_sell

--- stems_sell/stems_sell.py
+++ stems_sell/stems_sell.py
@@ -1,1 +1,26 @@
- 
+#!/usr/bin/env python3
+import fileinput
+
+def match(p, s):
+    print(p, '\n', s, '\n') 
+
+def main():
+    lineCount = 0
+    patterns = []
+    sentence  = ""
+    expectPattern = True
+    for line in fileinput.input(): 
+        if line == '\n' or line == '***':
+            expectPattern = not expectPattern
+            if sentence != "":
+                match(patterns, sentence)
+                patterns = []
+                sentence = ""
+        elif expectPattern:
+            patterns.append(line)
+        else:
+            sentence += line
+
+
+
+main()
