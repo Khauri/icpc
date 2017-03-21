9774bc476630fb72a89ca374846bddf438f9797e
Khauri McClain <Khauri McClain>
2017-03-21 13:39:13 -0400

    fixed some things

--- stems_sell/stems_sell.py
+++ stems_sell/stems_sell.py
@@ -1,18 +1,35 @@
 #!/usr/bin/env python3
 import fileinput
 import re
-
-
+# An exercise in regular expressions
 def match(p, s):
-    rules = { 'C':'[^aeiou]', 'V':'[aeiou]', '*':r'\w+'}
     m = re.compile(r'([CV]\d)')
-    #*C2ies -> \w+(^aeiou){2,2}ies
-    #print(p, '\n', s, '\n')
-    print("before: ", s)
     for pattern in p:
-        pattern[0] = pattern[0].replace("*", rules["*"])
-        x = re.compile(pattern[0])
+        r = stemToRegex(pattern[0])
+        #print(pattern[0], r)
+        for match in r.finditer(s):
+            #print(match.group(0))
+            s = s[:match.start(0)] + "replacement" + s[match.end(0):]
+            print(match.group(0))
+        #print('\n')
+    #print(s)
 
+"""
+Converts stem format to regex
+"""
+def stemToRegex(stem):
+    stemsReg = re.compile(r'([C|V])(\d)')
+    replacements = {"C" : "([^aeiou])\\", "V": "([aeiou])\\"}
+    any = r'\w+'
+    backref = 1
+    for s in stemsReg.finditer(stem):
+        repl = replacements[s.group(1)]
+        track = "{"+str(int(s.group(2)) - 1)+"}"
+        stem = stem[:s.start(0)]+ repl + str(backref) + track + stem[s.end(0):]
+        backref += 1
+    stem = stem.replace("*", any)
+    return re.compile(stem)
+    
 def main():
     lineCount = 0
     patterns = []
@@ -30,9 +47,6 @@ def main():
             patterns.append(line)
         else:
             sentence += line
-
-
-
 
 if __name__ == "__main__":
     main()
