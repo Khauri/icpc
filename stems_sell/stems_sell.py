88684854a97098e7dd3ec44a0574cc65818f0c86
Khauri McClain <Khauri McClain>
2017-03-21 15:57:59 -0400

    finished

--- stems_sell/stems_sell.py
+++ stems_sell/stems_sell.py
@@ -2,17 +2,13 @@
 import fileinput
 import re
 # An exercise in regular expressions
-def match(p, s):
+def match(patterns, s):
     m = re.compile(r'([CV]\d)')
-    for pattern in p:
-        r = stemToRegex(pattern[0])
-        #print(pattern[0], r)
-        for match in r.finditer(s):
-            #print(match.group(0))
-            s = s[:match.start(0)] + "replacement" + s[match.end(0):]
-            print(match.group(0))
-        #print('\n')
-    #print(s)
+    for pattern in patterns:
+        p = stemToRegex(pattern[0])
+        r = ruleToRegex(pattern[1])
+        s = re.sub(p, r, s)
+    return s
 
 """
 Converts stem format to regex
@@ -20,16 +16,21 @@ Converts stem format to regex
 def stemToRegex(stem):
     stemsReg = re.compile(r'([C|V])(\d)')
     replacements = {"C" : "([^aeiou])\\", "V": "([aeiou])\\"}
-    any = r'\w+'
+    any = r'(\w+)'
     backref = 1
+    if(stem.find('*') > -1):
+        stem = stem.replace("*", any)
+        backref += 1
     for s in stemsReg.finditer(stem):
         repl = replacements[s.group(1)]
         track = "{"+str(int(s.group(2)) - 1)+"}"
         stem = stem[:s.start(0)]+ repl + str(backref) + track + stem[s.end(0):]
         backref += 1
-    stem = stem.replace("*", any)
-    return re.compile(stem)
-    
+    return stem
+
+def ruleToRegex(rule):
+    return re.sub(r'(\d)', r'\\\1',rule)
+
 def main():
     lineCount = 0
     patterns = []
@@ -39,7 +40,7 @@ def main():
         if line == '\n' or line == '***':
             expectPattern = not expectPattern
             if sentence != "":
-                match(patterns, sentence)
+                print(match(patterns, sentence), end="***\n")
                 patterns = []
                 sentence = ""
         elif expectPattern:
