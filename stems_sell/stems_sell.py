b1142a7e7dad69fc7f43a29d4f58db0c7a8b5063
Khauri McClain <Khauri McClain>
2017-03-22 12:48:41 -0400

    added comments and changed structure

--- stems_sell/stems_sell.py
+++ stems_sell/stems_sell.py
@@ -1,53 +1,66 @@
 #!/usr/bin/env python3
+# An exercise in regular expressions
 import fileinput
 import re
-# An exercise in regular expressions
+
+"""
+Matches a list of pattern-repl lists and modifies a string s to match
+"""
 def match(patterns, s):
     m = re.compile(r'([CV]\d)')
     for pattern in patterns:
-        p = stemToRegex(pattern[0])
-        r = ruleToRegex(pattern[1])
-        s = re.sub(p, r, s)
+        p, r = stemToRegex(pattern[0], pattern[1])
+        s = re.sub(p, r, s, flags=re.IGNORECASE)
     return s
 
 """
 Converts stem format to regex
+CONVERSION RULES:
+  rule:
+    *  => '(\w+)'
+    Cn => '([^aeiou])\1{$n-1$}' ; 1 <= n <= 9
+    Vn => '([aeiou])\1{$n-1$}' ; 1 <= n <= 9
+  replacement:
+    n  => '\n' ; 1 <= n <= 9 
 """
-def stemToRegex(stem):
+def stemToRegex(matchRule, replacement):
     stemsReg = re.compile(r'([C|V])(\d)')
-    replacements = {"C" : "([^aeiou])\\", "V": "([aeiou])\\"}
+    replacements = {"C" : "[^aeiou]", "V": "[aeiou]"}
     any = r'(\w+)'
+    # convert replacement rule
+    replacement = re.sub(r'(\d)', r'\\\1', replacement)
+    # convert match rule
     backref = 1
-    if(stem.find('*') > -1):
-        stem = stem.replace("*", any)
+    if(matchRule.find('*') > -1):
+        matchRule = matchRule.replace("*", any)
         backref += 1
-    for s in stemsReg.finditer(stem):
-        repl = replacements[s.group(1)]
-        track = "{"+str(int(s.group(2)) - 1)+"}"
-        stem = stem[:s.start(0)]+ repl + str(backref) + track + stem[s.end(0):]
+    for s in stemsReg.finditer(matchRule):
+        a = "({0})\\{1}{{{2}}}".format(replacements[s.group(1)], str(backref), str(int(s.group(2)) - 1))
+        matchRule = matchRule[:s.start(0)]+ a + matchRule[s.end(0):]
         backref += 1
-    return stem
+    return [matchRule, replacement]
 
-def ruleToRegex(rule):
-    return re.sub(r'(\d)', r'\\\1',rule)
-
+"""
+Reads input from a file, performs matches, prints
+"""
 def main():
-    lineCount = 0
     patterns = []
     sentence  = ""
     expectPattern = True
-    for line in fileinput.input(): 
+    for line in fileinput.input(): # read input
         if line == '\n' or line == '***':
             expectPattern = not expectPattern
             if sentence != "":
+                # run match algorithm
                 print(match(patterns, sentence), end="***\n")
+                # reset pattern list and sentence
                 patterns = []
                 sentence = ""
-        elif expectPattern:
-            line = line.strip().split(" => ")
+        elif expectPattern: # collect patterns into patterns list
+            line = line.strip().split(" => ") # seperate pattern from replacement rule
             patterns.append(line)
         else:
-            sentence += line
+            sentence += line # collect sentence/paragraph line by line
 
 if __name__ == "__main__":
     main()
